a760b2e95d0cf73d82116bd4c52f8e976681b624
Lucas Brist <lucastbrist@gmail.com>
2025-02-12 18:50:16 -0600

    dependency injection

---
+++ src/main/java/com/codingnomads/ioc/examples/constructorinjection/HardDrive.java
@@ -0,0 +1,15 @@
+package com.codingnomads.ioc.examples.constructorinjection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+
+public class HardDrive {
+    private String name;
+    private String brand;
+
+}
