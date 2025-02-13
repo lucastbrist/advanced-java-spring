727c7278c1c003ea1b845f29519a7ef01fa63c3e
Lucas Brist <lucastbrist@gmail.com>
2025-02-13 17:44:28 -0600

    more spring labs

---
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/Coffee.java
@@ -0,0 +1,15 @@
+package com.codingnomads.corespring.examples.application_context.xml;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+
+public class Coffee {
+
+    private String name;
+
+}
