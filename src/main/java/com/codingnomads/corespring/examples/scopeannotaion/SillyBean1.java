da71e5777ef10dc9f736cd3dde16c38b7d8f00f5
Lucas Brist <lucastbrist@gmail.com>
2025-02-27 21:58:07 -0600

    more spring labs

---
+++ src/main/java/com/codingnomads/corespring/examples/scopeannotaion/SillyBean1.java
@@ -0,0 +1,9 @@
+package com.codingnomads.corespring.examples.scopeannotaion;
+
+public class SillyBean1 {
+
+    public SillyBean1() {
+        System.out.println("I'm a silly string bean--not to be confused with silly string.");
+    }
+
+}
---
+++ src/main/java/com/codingnomads/corespring/examples/scopeannotaion/SillyBean2.java
@@ -0,0 +1,7 @@
+package com.codingnomads.corespring.examples.scopeannotaion;
+
+public class SillyBean2 {
+    public SillyBean2() {
+        System.out.println("i am also printing something silly");
+    }
+}
