6cf4ceb47c02638b4653f77cca95ec2ad93496ee
Lucas Brist <lucastbrist@gmail.com>
2025-07-11 13:54:55 -0500

    lbd

--- src/main/java/com/codingnomads/aspectorientedprogramming/aop/service/StudentService.java
+++ src/main/java/com/codingnomads/aspectorientedprogramming/aop/service/StudentService.java
@@ -1,6 +1,8 @@
 /* CodingNomads (C)2024 */
 package com.codingnomads.aspectorientedprogramming.aop.service;
 
+import com.codingnomads.aspectorientedprogramming.aop.aspect.Loggable;
+import com.codingnomads.aspectorientedprogramming.aop.aspect.TrackMethodExecutionTime;
 import com.codingnomads.aspectorientedprogramming.aop.model.Student;
 import com.codingnomads.aspectorientedprogramming.aop.repository.StudentRepository;
 import java.util.List;
@@ -13,14 +15,14 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    // @TrackMethodExecutionTime
-    // @Loggable
+    @TrackMethodExecutionTime
+    @Loggable
     public List<Student> fetchAllStudents() {
         return studentRepository.findAll();
     }
 
-    // @TrackMethodExecutionTime
-    // @Loggable
+    @TrackMethodExecutionTime
+    @Loggable
     public List<Student> saveAllStudents(List<Student> students) {
         return studentRepository.saveAll(students);
     }
